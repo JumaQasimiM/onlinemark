bee5c78b9fe5d200b86eb313d5d77c87667e6132
M Juma <qasimimohammad819@gmail.com>
2025-11-29 22:12:00 +0100

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex">
+      <h2>Title</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+        repellat quibusdam nesciunt, quaerat odio ea praesentium! Tempore a
+        exercitationem ut dolor delectus atque, modi id quas quis, aut neque
+        animi?
+      </p>
+    </div>
+  );
+}
+
+export default App;
